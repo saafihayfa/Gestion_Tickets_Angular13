5f539da483a689c4ee8e61c2040d7115942d9257
saafi hayfa <saafihay109@gmail.com>
2022-06-21 14:43:46 +0200

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { LoginComponent } from './login/login.component';
 const routes: Routes = [
-  { path: "navbar", component: NavbarComponent }
+  { path: "navbar", component: NavbarComponent },
+  { path: "login", component: LoginComponent},
+  {path: "", redirectTo: "", pathMatch: "full" }
 ];
 
 @NgModule({
